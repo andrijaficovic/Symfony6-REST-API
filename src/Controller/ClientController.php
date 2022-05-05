cc28e5f1af60a2b7be9bcbf16a03a2f6595100aa
andrijaficovic <andrijaficovic45@gmail.com>
2022-05-05 18:47:39 +0200

    zavrsen crud za sve entite

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -23,30 +24,104 @@ class ClientController extends AbstractFOSRestController
         $this->em = $em;
     }
 
-    #[Rest\Get('/api/clients', name: 'get_clients')]
-   public function getClientsAction()
-   {
+    //Retrieve all clients
+    public function indexAction()
+    {
         $clients = $this->em->getRepository(Client::class)->findAll();
-        if($clients === null)
+        if(!$clients)
         {
-            return new View('There are no clients exist', Response::HTTP_NOT_FOUND);
+            throw new NotFoundHttpException('Clients not found');
+        }
+        return $this->handleView($this->view($clients, Response::HTTP_OK));
+    }
+
+    //retrieve client by id
+    public function showAction(Request $request)
+    {
+        $clientId = $request->get('clientId');
+        $client = $this->em->getRepository(Client::class)->find($clientId);
+        if(!$client)
+        {
+            throw new NotFoundHttpException('Requested client does not exist');
         }
 
-        return $this->view($clients,Response::HTTP_OK);
-   }
+        return $this->handleView($this->view($client, Response::HTTP_OK));
+    }
 
-   #[Rest\Get('/api/clients/{id}', name: 'get_client')]
-   public function getClientAction($id)
-   {
-       $client = $this->em->getRepository(Client::class)->find($id);
-       if($client === null)
-       {
-           return new View('The requested result does not exist', Response::HTTP_NOT_FOUND);
-       }
+    //create client record
+    public function createAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        $name = $data['name'];
+        $companyRegistrationNumber = $data['company_registration_number'];
+        $tin = $data['tin'];
+        $website = $data['website'];
 
-       return $this->view($client,Response::HTTP_OK);
-   }
+        //check if data is set
+        //website can be null
+        if(empty($name) || empty($companyRegistrationNumber) || empty($tin))
+        {
+            throw new BadRequestException('Fields can not be blank');
+        }
 
-    /*ODGLEDAJ VIDEO NA YT I KREIRAJ POST I PUT POMOCU FORMI,
-    I NAPRAVI ABSTRACTAPICONTROLLER*/
+        //inserting record in database
+        $client = new Client();
+        $client->setName($name);
+        $client->setCompanyRegistrationNumber($companyRegistrationNumber);
+        $client->setTin($tin);
+        $client->setWebsite($website);
+        $this->em->persist($client);
+        $this->em->flush();
+
+        return $this->handleView($this->view($client, Response::HTTP_CREATED));
+    }
+
+    //update client record
+    public function updateAction(Request $request)
+    {
+        $clientId = $request->get('clientId');
+        $client = $this->em->getRepository(Client::class)->find($clientId);
+        if(!$client)
+        {
+            throw new NotFoundHttpException('Requested client does not exist');
+        }
+        $data = json_decode($request->getContent(), true);
+        $name = $data['name'];
+        $companyRegistrationNumber = $data['company_registration_number'];
+        $tin = $data['tin'];
+        $website = $data['website'];
+
+        //check if data is set
+        //website can be null
+        if(empty($name) || empty($companyRegistrationNumber) || empty($tin))
+        {
+            throw new BadRequestException('Fields can not be blank');
+        }
+
+        //inserting record in database
+        $client->setName($name);
+        $client->setCompanyRegistrationNumber($companyRegistrationNumber);
+        $client->setTin($tin);
+        $client->setWebsite($website);
+        $this->em->persist($client);
+        $this->em->flush();
+
+        return $this->handleView($this->view($client, Response::HTTP_CREATED));
+    }
+
+    //Delete address record
+    public function deleteAction(Request $request)
+    {
+        $clientId = $request->get('clientId');
+        $client = $this->em->getRepository(Client::class)->find($clientId);
+        if(!$client)
+        {
+            throw new NotFoundHttpException('Requested client does not exist');
+        }
+
+        $this->em->remove($client);
+        $this->em->flush();
+
+        return $this->handleView($this->view('Client successfully deleted', Response::HTTP_OK));
+    }
 }
