a57483fb5936c7a9406381087733e68614a47385
andrijaficovic <andrijaficovic45@gmail.com>
2022-05-08 11:11:06 +0200

    instaliran jwt lexik bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
