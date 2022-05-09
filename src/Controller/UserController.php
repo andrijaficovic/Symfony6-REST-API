080fa79c07fefb5780643c5f350f849dbf34e392
andrijaficovic <andrijaficovic45@gmail.com>
2022-05-09 23:03:49 +0200

    commit pre konacne provere

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends AbstractFOSRestController
@@ -23,12 +27,16 @@ class UserController extends AbstractFOSRestController
     public function indexAction(Request $request)
     {
         $userId = $request->get('userId');
-
-        $user = $this->em->getRepository(User::class)->find($userId);
-        if(!$user)
+        $currentUser = $this->getUser()->getId();
+        if(!($userId == $currentUser))
         {
-            throw new NotFoundHttpException('User not found');
+            throw new AccessDeniedException('Access denied', Response::HTTP_CONFLICT);
         }
 
+        $clients = $this->em->getRepository(Client::class)->findBy([
+            'user'=>$userId
+        ]);
+
+        return $this->view($clients);
     }
 }
